dcb8e80f77d8cfc64c146e81aaf40e4cd3e3113e
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-09 12:57:57 -0500

    Created ComicPost Model

---
+++ database/migrations/2024_01_09_175605_create_comic_posts_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comic_posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('comic_series_id')->constrained('comic_series')->onDelete('cascade')->nullable();
+            $table->integer('page_number')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comic_posts');
+    }
+};
