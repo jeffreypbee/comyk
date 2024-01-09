ef46600e47baba4265f6b311ce62837d781825cf
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-09 12:55:38 -0500

    Created ComicSeries Model

---
+++ app/Models/ComicSeries.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ComicSeries extends Model
+{
+    use HasFactory;
+}
