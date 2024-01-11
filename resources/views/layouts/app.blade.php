c4744d6ab59310108220ec25fbe15a79aa616c9d
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-11 12:05:48 -0500

    Starting frontend layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,23 +20,18 @@
     <body class="font-sans antialiased">
         <x-banner />
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+        @include('layouts.partials.header')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+        <div class="min-h-screen bg-gray-100">
+            {{-- @livewire('navigation-menu') --}}
 
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
         </div>
+
+        @include('layouts.partials.footer')
 
         @stack('modals')
 
