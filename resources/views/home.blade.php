782fae557af98d349e7bc95cbe4f49f7de4ca4cd
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-11 12:35:42 -0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,21 @@
 <x-app-layout>
-    <h3>Latest Blog Post</h3>
-    <div>{{$latestBlogPost->title}}</div>
 
-    <h3>Latest Art Post</h3>
-    <div>{{$latestArtPost->title}}</div>
+    <div class="flex gap-5 justify-around">
+        <div class="p-8 rounded-lg border-2 border-black ">
+            <h3>Latest Blog Post</h3>
+            <div>{{$latestBlogPost->title}}</div>
+        </div>
 
-    <h3>Latest Comic Post</h3>
-    <div>{{$latestComicPost->title}}</div>
+        <div class="p-8 rounded-lg border-2 border-black ">
+            <h3>Latest Comic Post</h3>
+            <div>{{$latestComicPost->title}}</div>
+        </div>
+
+        <div class="p-8 rounded-lg border-2 border-black ">
+            <h3>Latest Art Post</h3>
+            <div>{{$latestArtPost->title}}</div>
+        </div>
+    </div>
 
     <h3>Latest Posts</h3>
     @foreach ($latestPosts as $post)
