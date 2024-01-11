cd96997f29dab4895066e062239fd4d54d9129b1
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-11 12:24:53 -0500

    Added latest post for each category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\ArtPost;
+use App\Models\BlogPost;
+use App\Models\ComicPost;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,7 +13,10 @@ class HomeController extends Controller
     public function __invoke()
     {
         return view('home', [
-            'latestPosts' => Post::published()->latest('publish_date')->get()
+            'latestPosts' => Post::published()->latest('publish_date')->take(10)->get(),
+            'latestBlogPost' => Post::published()->where('postable_type', BlogPost::class)->latest('publish_date')->first(),
+            'latestArtPost' => Post::published()->where('postable_type', ArtPost::class)->latest('publish_date')->first(),
+            'latestComicPost' => Post::published()->where('postable_type', ComicPost::class)->latest('publish_date')->first()
         ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,13 @@
 <x-app-layout>
+    <h3>Latest Blog Post</h3>
+    <div>{{$latestBlogPost->title}}</div>
+
+    <h3>Latest Art Post</h3>
+    <div>{{$latestArtPost->title}}</div>
+
+    <h3>Latest Comic Post</h3>
+    <div>{{$latestComicPost->title}}</div>
+
     <h3>Latest Posts</h3>
     @foreach ($latestPosts as $post)
       <div>{{$post->title}}</div>
