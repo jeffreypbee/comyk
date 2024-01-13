5a96b84079d657c18622582dfa182862e48ae691
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-13 12:44:17 -0500

    Created latest-post-card component

---
+++ resources/views/components/site/latest-post-card.blade.php
@@ -0,0 +1,5 @@
+@props(['post'])
+
+<div class="p-8 rounded-lg border-2 border-black ">
+    <div>{{$post->title}}</div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,9 @@
 <x-app-layout>
 
     <div class="flex gap-5 justify-around">
-        <div class="p-8 rounded-lg border-2 border-black ">
-            <h3>Latest Blog Post</h3>
-            <div>{{$latestBlogPost->title}}</div>
-        </div>
-
-        <div class="p-8 rounded-lg border-2 border-black ">
-            <h3>Latest Comic Post</h3>
-            <div>{{$latestComicPost->title}}</div>
-        </div>
-
-        <div class="p-8 rounded-lg border-2 border-black ">
-            <h3>Latest Art Post</h3>
-            <div>{{$latestArtPost->title}}</div>
-        </div>
+        <x-site.latest-post-card :post="$latestBlogPost" />
+        <x-site.latest-post-card :post="$latestArtPost" />
+        <x-site.latest-post-card :post="$latestComicPost" />
     </div>
 
     <h3>Latest Posts</h3>
