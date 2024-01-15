a5d3f4c705b65cdb7082b87a49431c775c048f01
Jeffrey Bee <jeffreypbee@gmail.com>
2024-01-15 11:27:54 -0500

    Styling post card

--- resources/views/components/site/latest-post-card.blade.php
+++ resources/views/components/site/latest-post-card.blade.php
@@ -2,6 +2,6 @@
 
 <div class="rounded-lg bg-white w-1/4"
     style="background: url({{$post->image}})">
-    <div class="p-1 border-b">{{$post->getPostType()}}</div>
+    <div class="p-1 border-b bg-white">{{$post->getPostType()}}</div>
     <div class="font-bold p-10">{{$post->title}}</div>
 </div>
--- resources/views/components/site/post-card.blade.php
+++ resources/views/components/site/post-card.blade.php
@@ -2,6 +2,6 @@
 
 <div class="bg-white rounded-lg w-48"
     style="background: url({{$post->image}})">
-    <div class="p-1 border-b">{{$post->getPostType()}}</div>
+    <div class="p-1 border-b bg-white">{{$post->getPostType()}}</div>
     <div class="font-bold p-10">{{$post->title}}</div>
 </div>
